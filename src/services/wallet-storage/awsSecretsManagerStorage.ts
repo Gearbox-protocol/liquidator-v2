d0c488cf5f8ba961f2ac6d810579ed9dca12bd19
Konstantin Kuznetsov <K.Kuznetcov@gmail.com>
2022-09-26 14:20:01 +0300

    fix: array check, ts cache

--- src/services/wallet-storage/awsSecretsManagerStorage.ts
+++ src/services/wallet-storage/awsSecretsManagerStorage.ts
@@ -28,11 +28,13 @@ export default class AwsSecretsManagerStorage implements IWalletStorage {
         throw new Error("secret not found");
       }
       const keys = JSON.parse(SecretString);
-      if (Array.isArray(keys)) {
+      if (!Array.isArray(keys)) {
         throw new Error("secret must be an array");
       }
       this.encryptedWallets = keys;
+      console.info(`Loaded ${keys.length} keys`);
     } catch (e) {
+      console.error(`Cant get keys from AWS secrets manager: ${e}`);
       this.ampqService.error(`Cant get keys from AWS secrets manager: ${e}`);
       process.exit(1);
     }
@@ -50,6 +52,7 @@ export default class AwsSecretsManagerStorage implements IWalletStorage {
           config.walletPassword,
         );
       } catch (e) {
+        console.error(`Cant get key from: ${e}`);
         this.ampqService.error(`Cant get key from: ${e}`);
         process.exit(1);
       }
@@ -65,8 +68,10 @@ export default class AwsSecretsManagerStorage implements IWalletStorage {
           SecretString: JSON.stringify(this.encryptedWallets),
         }),
       );
+      console.info("Pushed new key");
       return newWallet;
     } catch (e) {
+      console.error(`Cant create new key: ${e}`);
       this.ampqService.error(`Cant create new key: ${e}`);
       process.exit(1);
     }
