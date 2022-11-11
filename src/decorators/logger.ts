39490e22962ef9f1c8f69fa7b387df2c95a29b74
doomsower <K.Kyznetcov@gmail.com>
2022-11-11 18:10:19 +0300

    feat: use pino for producition logging

--- src/decorators/logger.ts
+++ src/decorators/logger.ts
@@ -1,13 +1,11 @@
-import {
-  Logger as TSLogger,
-  LoggerWithoutCallSite,
-  TLogLevelName,
-} from "tslog";
+import { pino } from "pino";
+import { Logger as TSLogger, TLogLevelName } from "tslog";
 import { Container } from "typedi";
 
 const DEV = process.env.NODE_ENV !== "production";
-
-const LoggerClass = DEV ? TSLogger : LoggerWithoutCallSite;
+const underlying = process.env.UNDERLYING;
+// For optimistic liquidators in AWS StepFunctions
+const executionId = process.env.EXECUTION_ID?.split(":").pop();
 
 export function Logger(label?: string): PropertyDecorator {
   return (target: any, propertyKey): any => {
@@ -16,17 +14,28 @@ export function Logger(label?: string): PropertyDecorator {
       object: target,
       propertyName,
       value: () =>
-        new LoggerClass({
-          type: DEV ? "pretty" : "json",
-          ignoreStackLevels: DEV ? 3 : 100,
-          hostname: undefined,
-          name: label,
-          displayFunctionName: false,
-          displayLoggerName: false,
-          displayFilePath: "hidden",
-          minLevel: (process.env.LOG_LEVEL as TLogLevelName) ?? "debug",
-          prefix: process.env.UNDERLYING ? [process.env.UNDERLYING] : undefined,
-        }),
+        DEV
+          ? new TSLogger({
+              type: "pretty",
+              hostname: undefined,
+              name: label,
+              displayFunctionName: false,
+              displayLoggerName: false,
+              displayFilePath: "hidden",
+              minLevel: (process.env.LOG_LEVEL as TLogLevelName) ?? "debug",
+              prefix: underlying ? [underlying] : undefined,
+            })
+          : pino({
+              level: process.env.LOG_LEVEL ?? "debug",
+              base: { underlying, executionId },
+              formatters: {
+                level: label => {
+                  return {
+                    level: label,
+                  };
+                },
+              },
+            }),
     });
   };
 }
