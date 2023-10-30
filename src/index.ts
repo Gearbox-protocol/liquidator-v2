693fcf3f5d05937da15678f902362a7727619b08
doomsower <K.Kuznetcov@gmail.com>
2023-10-30 12:06:47 -0300

    fix: print version

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@
 
 import "reflect-metadata";
 
+import path from "node:path";
+
 import { launchApp } from "./app";
 import config from "./config";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const version = require(path.resolve(__dirname, "../package.json")).version;
+console.log("Liquidator TS version: " + version);
 
 process.on("uncaughtException", e => {
   console.log(e);
