8f788c561eb58502811f7b4812938b08b5f20ca9
doomsower <12031673+doomsower@users.noreply.github.com>
2024-04-27 14:24:15 -0400

    fix: do not use retry provider in optimistic

--- src/services/utils/getProvider.ts
+++ src/services/utils/getProvider.ts
@@ -2,11 +2,19 @@ import {
   RetryProvider,
   RotateProvider,
 } from "@gearbox-protocol/devops/lib/providers";
+import { providers } from "ethers";
 
 import config from "../../config";
 import type { LoggerInterface } from "../../log";
 
 export function getProvider(flashbots = false, logger?: LoggerInterface) {
+  if (config.optimisticLiquidations) {
+    return new providers.StaticJsonRpcProvider({
+      url: config.ethProviderRpcs[0],
+      timeout: 120_000,
+      allowGzip: true,
+    });
+  }
   const rpcs = config.ethProviderRpcs.map(
     url =>
       new RetryProvider(
