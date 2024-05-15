71e809f4f88aebec3b9affd4638801c0421c8031
doomsower <12031673+doomsower@users.noreply.github.com>
2024-05-15 12:39:18 -0400

    fix: round realtime delta

--- src/services/RedstoneServiceV3.ts
+++ src/services/RedstoneServiceV3.ts
@@ -110,9 +110,11 @@ export class RedstoneServiceV3 {
         throw new Error("cannot get latest block");
       }
       const delta = block.timestamp - redstoneTs;
-      const realtimeDelta = new Date().getTime() / 1000 - redstoneTs;
+      const realtimeDelta = Math.floor(
+        new Date().getTime() / 1000 - redstoneTs,
+      );
       this.log.debug(
-        `redstone delta ${delta} (realtime delta ${realtimeDelta}) for timestamp ${formatTs(block)}: ${formatTs(redstoneTs)}`,
+        `redstone delta ${delta} (realtime ${realtimeDelta}) for timestamp ${formatTs(block)}: ${formatTs(redstoneTs)}`,
       );
       if (delta < 0) {
         this.log?.debug(
