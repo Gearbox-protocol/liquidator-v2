78f87cb9a19bc6e15b8c1723268e8ba095571c22
doomsower <12031673+doomsower@users.noreply.github.com>
2024-05-20 13:22:10 -0400

    fix: top up balance during impersonation

--- src/utils/impersonate.ts
+++ src/utils/impersonate.ts
@@ -3,14 +3,17 @@ import type { JsonRpcProvider, JsonRpcSigner, Provider } from "ethers";
 export async function impersonate(
   provider: Provider,
   address: string,
+  topUpBalance = true,
 ): Promise<JsonRpcSigner> {
   await (provider as JsonRpcProvider).send("anvil_impersonateAccount", [
     address,
   ]);
-  // await (provider as JsonRpcProvider).send("anvil_setBalance", [
-  //   address,
-  //   "0x10000000000000000000",
-  // ]);
+  if (topUpBalance) {
+    await (provider as JsonRpcProvider).send("anvil_setBalance", [
+      address,
+      "0x10000000000000000000",
+    ]);
+  }
   return (provider as JsonRpcProvider).getSigner(address);
 }
 
