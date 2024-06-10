949b03bfa48f2a702ad4a8d75fa212711b10e2e7
doomsower <12031673+doomsower@users.noreply.github.com>
2024-06-10 16:13:44 -0400

    fix: chain for viem

--- src/utils/getViemPublicClient.ts
+++ src/utils/getViemPublicClient.ts
@@ -1,19 +1,31 @@
+import type { NetworkType } from "@gearbox-protocol/sdk-gov";
 import { Container, Token } from "typedi";
-import { createPublicClient, fallback, http, type PublicClient } from "viem";
+import type { Chain, PublicClient } from "viem";
+import { createPublicClient, defineChain, fallback, http } from "viem";
+import { arbitrum, base, mainnet, optimism } from "viem/chains";
 
 import type { Config } from "../config/index.js";
 import { CONFIG } from "../config/index.js";
 
 export const VIEM_PUBLIC_CLIENT = new Token("viemPublicClient");
 
+const CHAINS: Record<NetworkType, Chain> = {
+  Mainnet: mainnet,
+  Arbitrum: arbitrum,
+  Optimism: optimism,
+  Base: base,
+};
+
 export function getViemPublicClient(): PublicClient {
-  const config = Container.get(CONFIG) as Config;
-  const rpcs = config.ethProviderRpcs.map(url =>
-    http(url, { timeout: 120_000 }),
-  );
+  const { ethProviderRpcs, chainId, network } = Container.get(CONFIG) as Config;
+  const rpcs = ethProviderRpcs.map(url => http(url, { timeout: 120_000 }));
 
   return createPublicClient({
     cacheTime: 0,
+    chain: defineChain({
+      ...CHAINS[network],
+      id: chainId,
+    }),
     transport: rpcs.length > 1 ? fallback(rpcs) : rpcs[0],
   });
 }
