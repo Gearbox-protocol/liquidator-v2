2d0aa373e9cbf6b2bb178cfe180ed1bb4519e084
doomsower <12031673+doomsower@users.noreply.github.com>
2024-07-15 08:04:52 -0500

    fix: remove pino-pretty from build

--- src/log/index.ts
+++ src/log/index.ts
@@ -1,9 +1,6 @@
 import type { IFactory } from "di-at-home";
-import { createRequire } from "module";
-import type { DestinationStream, Logger as ILogger, LoggerOptions } from "pino";
+import type { Logger as ILogger } from "pino";
 import { pino } from "pino";
-
-const require = createRequire(import.meta.url);
 
 import { DI } from "../di.js";
 
@@ -13,7 +10,7 @@ class LoggerFactory implements IFactory<ILogger, [string]> {
 
   constructor() {
     const executionId = process.env.EXECUTION_ID?.split(":").pop();
-    const options: LoggerOptions = {
+    this.#logger = pino({
       level: process.env.LOG_LEVEL ?? "debug",
       base: { executionId },
       formatters: {
@@ -25,18 +22,7 @@ class LoggerFactory implements IFactory<ILogger, [string]> {
       },
       // fluent-bit (which is used in our ecs setup with loki) cannot handle unix epoch in millis out of the box
       timestamp: () => `,"time":${Date.now() / 1000.0}`,
-    };
-    let stream: DestinationStream | undefined;
-    // this label will be dropped by esbuild during production build
-    // eslint-disable-next-line no-labels
-    DEV: {
-      // eslint-disable-next-line @typescript-eslint/no-require-imports
-      const pinoPretty = require("pino-pretty");
-      stream = pinoPretty({
-        colorize: true,
-      });
-    }
-    this.#logger = pino(options, stream);
+    });
   }
 
   public produce(name: string): ILogger {
