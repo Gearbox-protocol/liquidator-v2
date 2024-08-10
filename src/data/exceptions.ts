f45bc6277637d66fd04d7d76502b0a4b7a3765ad
doomsower <12031673+doomsower@users.noreply.github.com>
2024-08-10 08:25:16 -0400

    fix: decode redstone errors

--- src/data/exceptions.ts
+++ src/data/exceptions.ts
@@ -1,11 +1,13 @@
 import {
   iExceptionsAbi,
   ilpPriceFeedExceptionsAbi,
+  iRedstoneErrorsAbi,
   iRedstonePriceFeedExceptionsAbi,
 } from "@gearbox-protocol/types/abi";
 
 export const exceptionsAbis = [
   ...iExceptionsAbi,
   ...iRedstonePriceFeedExceptionsAbi,
+  ...iRedstoneErrorsAbi,
   ...ilpPriceFeedExceptionsAbi,
 ];
