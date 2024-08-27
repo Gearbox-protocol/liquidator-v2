22f609b608366496f4c46be85f6e1f36a196a03c
doomsower <12031673+doomsower@users.noreply.github.com>
2024-08-27 13:34:39 -0300

    fix: remove unused code

--- src/services/RedstoneServiceV3.ts
+++ src/services/RedstoneServiceV3.ts
@@ -473,17 +473,6 @@ function getCalldataWithTimestamp(
   };
 }
 
-function splitResponse<T>(arr: T[], size: number): T[][] {
-  const chunks = [];
-
-  for (let i = 0; i < arr.length; i += size) {
-    const chunk = arr.slice(i, i + size);
-    chunks.push(chunk);
-  }
-
-  return chunks;
-}
-
 function printFeeds(feeds: RedstoneFeed[]): string {
   return feeds
     .map(
