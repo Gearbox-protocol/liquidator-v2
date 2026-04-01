a2f122687990285d8812ebef644f07cc0876a793
doomsower <12031673+doomsower@users.noreply.github.com>
2026-04-01 15:04:00 -0300

    fix: add resilient and log spltting transport

--- src/utils/createTransport.ts
+++ src/utils/createTransport.ts
@@ -1,9 +1,15 @@
 import {
   createRevolverTransport,
+  getProviders,
   type INotificationService,
 } from "@gearbox-protocol/cli-utils";
 import type { ILogger } from "@gearbox-protocol/sdk";
-import type { Transport } from "viem";
+import {
+  logSplitterTransport,
+  RevolverTransport,
+  resilientTransport,
+} from "@gearbox-protocol/sdk/dev";
+import { http, type Transport } from "viem";
 import type { CommonSchema } from "../config/common.js";
 import {
   ProviderRotationErrorNotification,
@@ -15,12 +21,25 @@ export function createTransport(
   logger: ILogger,
   notifier: INotificationService,
 ): Transport {
-  return createRevolverTransport(config, {
-    resilient: true,
-    defaultHTTPOptions: {
-      timeout: config.optimistic ? 240_000 : 10_000,
-      retryCount: config.optimistic ? 3 : undefined,
-    },
+  const providers = getProviders(config);
+
+  let transports = providers.map(
+    ({ url, name, httpTransportOptions }): Transport =>
+      http(url, {
+        timeout: config.optimistic ? 240_000 : 10_000,
+        retryCount: config.optimistic ? 3 : undefined,
+        ...httpTransportOptions,
+        key: name,
+        name: name,
+      }),
+  );
+
+  transports = transports.map(t => resilientTransport(t));
+
+  transports = transports.map(t => logSplitterTransport(t));
+
+  return RevolverTransport.create({
+    transports,
     logger: logger?.child?.({ name: "transport" }),
     onRotateSuccess: (oldT, newT, reason) => {
       notifier.notify(
