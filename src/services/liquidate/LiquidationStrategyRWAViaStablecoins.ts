c0998dbba9d875918289c58617562aa3f3ca4766
doomsower <12031673+doomsower@users.noreply.github.com>
2026-05-13 21:40:51 -0400

    fix: remove logs

--- src/services/liquidate/LiquidationStrategyRWAViaStablecoins.ts
+++ src/services/liquidate/LiquidationStrategyRWAViaStablecoins.ts
@@ -78,7 +78,7 @@ export default class LiquidationStrategyRWAViaStablecoins
   public async syncState(_blockNumber: bigint): Promise<void> {}
 
   public isApplicable(ca: CreditAccountData, _optimistic: boolean): boolean {
-    const ctx = resolveRWAContext(this.sdk, ca, this.logger);
+    const ctx = resolveRWAContext(this.sdk, ca);
     return !!ctx;
   }
 
@@ -89,7 +89,7 @@ export default class LiquidationStrategyRWAViaStablecoins
       throw new Error("makeLiquidatable only works in optimistic mode");
     }
     const cs = this.sdk.marketRegister.findCreditManager(ca.creditManager);
-    const ctx = resolveRWAContext(this.sdk, ca, this.logger);
+    const ctx = resolveRWAContext(this.sdk, ca);
     if (!ctx) {
       throw new Error(`Credit manager ${cs.name} is not an RWA credit manager`);
     }
@@ -256,7 +256,7 @@ export default class LiquidationStrategyRWAViaStablecoins
   }
 
   public async preview(ca: CreditAccountData): Promise<RWALiquidationPreview> {
-    const ctx = resolveRWAContext(this.sdk, ca, this.logger);
+    const ctx = resolveRWAContext(this.sdk, ca);
     const redemptionGateway = ctx?.gateway;
     if (!redemptionGateway) {
       throw new Error(
--- src/services/liquidate/rwa/resolveRWAContext.ts
+++ src/services/liquidate/rwa/resolveRWAContext.ts
@@ -1,7 +1,6 @@
 import {
   type CreditAccountData,
   hexEq,
-  type ILogger,
   type OnchainSDK,
   type SecuritizeRWAFactory,
 } from "@gearbox-protocol/sdk";
@@ -28,47 +27,28 @@ export interface RWAContext {
 export function resolveRWAContext(
   sdk: OnchainSDK,
   ca: CreditAccountData,
-  logger: ILogger,
 ): RWAContext | undefined {
   const meta = sdk.tokensMeta.mustGet(ca.underlying);
   if (!sdk.tokensMeta.isRWAUnderlying(meta)) {
-    logger.warn(
-      `Underlying ${sdk.labelAddress(ca.underlying)} is not an RWA underlying`,
-    );
     return undefined;
   }
   const factory = sdk.rwa.factories.find(f =>
     hexEq(f.address, meta.rwaFactory),
   );
   if (!factory) {
-    logger.error(
-      `RWA factory ${meta.rwaFactory} not found for underlying ${sdk.labelAddress(ca.underlying)}`,
-    );
     throw new Error(
       `RWA factory ${meta.rwaFactory} not found for underlying ${sdk.labelAddress(ca.underlying)}`,
     );
   }
-  logger.debug(
-    `Found RWA factory ${factory.address} for underlying ${sdk.labelAddress(ca.underlying)}`,
-  );
   const cm = sdk.marketRegister.findCreditManager(ca.creditManager);
   // look up by credit manager tokens, because we redeem all DSTokens during makeLiquidatable
   const dsToken = factory.dsTokens.find(ds =>
     cm.creditManager.collateralTokens.some(t => hexEq(t, ds.address)),
   );
   if (!dsToken) {
-    logger.warn(
-      `No DSToken found for underlying ${sdk.labelAddress(ca.underlying)}`,
-    );
     return undefined;
   }
-  logger.debug(
-    `Found DSToken ${sdk.labelAddress(dsToken.address)} for underlying ${sdk.labelAddress(ca.underlying)}`,
-  );
   const gateway = dsToken.operators[0];
-  logger.debug(
-    `Found gateway ${gateway} for DSToken ${sdk.labelAddress(dsToken.address)}`,
-  );
   if (!gateway) {
     throw new Error(
       `DS token ${sdk.labelAddress(dsToken.address)} has no operators registered in Securitize factory ${factory.address}`,
