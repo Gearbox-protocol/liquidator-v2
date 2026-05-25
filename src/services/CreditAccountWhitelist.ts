1a4eb9b14c385d7b67dbaf205c59913fa249a0bf
doomsower <12031673+doomsower@users.noreply.github.com>
2026-05-25 08:54:29 -0400

    fix: use tokens from whitelist

--- src/services/CreditAccountWhitelist.ts
+++ src/services/CreditAccountWhitelist.ts
@@ -10,6 +10,17 @@ export class CreditAccountWhitelist extends Whitelist {
    * is whitelisted.
    */
   public match(ca: CreditAccountData): WhitelistItem | undefined {
-    return this.has(ca.creditAccount) ?? this.has(ca.creditManager);
+    if (this.has(ca.creditAccount)) {
+      return this.has(ca.creditAccount);
+    }
+    if (this.has(ca.creditManager)) {
+      return this.has(ca.creditManager);
+    }
+    for (const t of ca.tokens) {
+      if (t.balance > 1n && this.has(t.token)) {
+        return this.has(t.token);
+      }
+    }
+    return undefined;
   }
 }
